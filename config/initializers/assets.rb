4b05fd759aa0b6339f5ed4a9887eaa6b9d27beb7
Bechir Segni <bechir.dev@gmail.com>
2015-07-08 02:52:14 +0100

    Fixing assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( ckeditor/* )
 Rails.application.config.assets.precompile += %w( admin.css , admin.js)
+Rails.application.config.assets.precompile += %w( style.css , main.js)
 
