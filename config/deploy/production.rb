967b52c2ac23d74928e64d086fbb4f3bb2168c32
Bechir Segni <bechir.dev@gmail.com>
2015-07-08 05:51:43 +0100

    add to another IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,4 +63,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '22.22.22.2', user: 'deploy', roles: %w{web app}
+server '74.208.133.145', user: 'root', roles: %w{web app}
