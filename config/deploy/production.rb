bf5ea85dd1b7e23731ef692459687e3cedc943f4
Bechir Segni <bechir.dev@gmail.com>
2015-07-09 02:30:03 +0100

    Deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,4 +63,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '74.208.133.145', user: 'root', roles: %w{web app}
+server '74.208.133.145', user: 'deploy', roles: %w{web app}
